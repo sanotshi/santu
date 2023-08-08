7729667a7e831affe16cd544e005ec3f4a1a72cb
sanotshi <140939440+sanotshi@users.noreply.github.com>
2023-08-08 20:08:00 +0530

    E-commerce website
    my new project.

---
+++ shopping.js
@@ -0,0 +1,33 @@
+
+const button=document.querySelector(".button")
+const button1=document.querySelector(".button1")
+const button2=document.querySelector(".button2")
+const button3=document.querySelector(".button3")
+const button4=document.querySelector(".button4")
+const payment=document.querySelector(".payment")
+const close=document.querySelector(".close")
+button.addEventListener("click",()=>{
+    payment.style.display="flex";
+    
+})
+button1.addEventListener("click",()=>{
+    payment.style.display="flex";
+    
+})
+button2.addEventListener("click",()=>{
+    payment.style.display="flex";
+    
+})
+button3.addEventListener("click",()=>{
+    payment.style.display="flex";
+    
+})
+button4.addEventListener("click",()=>{
+    payment.style.display="flex";
+    
+})
+close.addEventListener("click",()=>{
+    payment.style.display="none";
+    
+})
+
